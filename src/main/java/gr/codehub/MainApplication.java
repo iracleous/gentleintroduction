62144f56be33b1d8f091b79d1008786c50569bb2
dimitris <iracleous@gmail.com>
2021-11-29 21:12:19 +0200

    services

--- src/main/java/gr/codehub/MainApplication.java
+++ src/main/java/gr/codehub/MainApplication.java
@@ -5,40 +5,34 @@ import gr.codehub.model.Person;
 import gr.codehub.services.Registration;
 import gr.codehub.services.Validation;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainApplication {
 
 
     public static void main(String[] args) {
-        Person person = new Person();
         Registration registration = new Registration();
         Validation validation = new Validation();
 
-        if (validation.personValidation(person))
-            registration.register(person);
+       for(long i = 0; i <5; i++)
+        {
+            Person person = new Person();
+            person.setName("Danae");
+            Address address = new Address("Doridos", "7-9", 11526, "Thessaloniki");
+            person.setAddress(address);
+            person.setAge(20);
+            person.setIdentifier(i);
+            person.setRegistrationId((int) i);
+
+            if (validation.personValidation(person))
+                registration.register(person);
+        }
+
+        registration.showPersons();
+
+
+        System.out.println("Total balance of persons is "+ registration.calculateTotalBalance());
+
     }
-
-
-    public static void test1() {
-        Person person = new Person();
-        Person otherPerson = new Person();
-        otherPerson.setName("Danae");
-
-        Address address = new Address("Doridos", "7-9", 11526, "Athens");
-
-        otherPerson.setAddress(address);
-
-        otherPerson.setAge(20);
-        System.out.println("hello " + otherPerson);
-
-        Person thirdPerson = otherPerson;
-        thirdPerson.setAge(21);
-        System.out.println("hello " + otherPerson);
-
-        System.out.println("hello " + person);
-
-        System.out.println("hello " + thirdPerson);
-    }
-
 }
--- src/main/java/gr/codehub/services/Registration.java
+++ src/main/java/gr/codehub/services/Registration.java
@@ -2,16 +2,69 @@ package gr.codehub.services;
 
 import gr.codehub.model.Person;
 
+import java.util.ArrayList;
+
 public class Registration {
 
-    public void register(Person person){
+    private ArrayList<Person> persons = new ArrayList<>();
 
+
+    public void register(Person person){
+            persons.add(person);
     }
+
+
     public void hire(Person person){
 
     }
     public void dismiss(Person person){
 
+    }
+
+
+    public void showPersons(){
+        /*
+        for (int i = 0;i < persons.size(); i++){
+            System.out.println(persons.get(i));
+        }
+
+        for(Person person:persons){
+            System.out.println(person);
+        }
+
+        persons.forEach(person -> System.out.println(person));
+*/
+        persons.forEach(System.out::println);
+
+    }
+
+
+
+
+
+
+    public float calculateTotalBalance(){
+
+        float totalBalance = 0;
+//        for (int i = 0;i < persons.size(); i++){
+//            totalBalance += persons.get(i).getBalance();
+//        }
+
+
+//  imperative programming
+
+       for(Person person:persons){
+           totalBalance += person.getBalance();
+        }
+
+       //functional programming
+
+//        totalBalance = persons
+//                .stream()
+//                .map(person  -> person.getBalance())
+//                .reduce(0.0f, (a,b) -> a+b);
+
+        return totalBalance;
     }
 
 }
